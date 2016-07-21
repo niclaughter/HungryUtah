d984807e4d11cde10db07ff8981bbc4f98dfdaa7
AJ Bronson <bronson.aj@gmail.com>
2016-07-21 10:58:59 -0600

    implemented post functionality

--- FoodTruck/MapViewController.swift
+++ FoodTruck/MapViewController.swift
@@ -17,6 +17,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     let locationManager = CLLocationManager()
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var postButton: UIButton!
+    @IBOutlet var timePicker: UIDatePicker!
     
     @IBAction func centerMapOnUserLocationButtonTapped(sender: AnyObject) {
         guard let location = locationManager.location else { return }
@@ -70,4 +71,43 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             LocationManager.shared.updateMap(mapView, location: location, region: MKCoordinateRegionMakeWithDistance(location.coordinate, 50, 50))
         }
     }
+    
+    var dateTextField: UITextField?
+    
+    @IBAction func postButtonTapped(sender: UIButton) {
+        let alert = UIAlertController(title: "Select Departure Time", message: "What time do you plan on packing up and leaving this area?", preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Departure Time"
+            textField.inputView = self.timePicker
+            self.timePicker.minimumDate = NSDate()
+            self.dateTextField = textField
+        }
+        let doneAction = UIAlertAction(title: "Done", style: .Default) { (_) in
+            guard let textFields = alert.textFields,
+                let textField = textFields.first,
+                let location = self.locationManager.location,
+                let text = textField.text where text.characters.count > 0 else { self.showAlert("Error Saving Post", message: "You either didn't provide a date, or we couldn't find your current location."); return }
+            PostController.postLocation(location, expirationDate: self.timePicker.date)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        alert.addAction(doneAction)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func timePickerChanged(sender: UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "hh:mm a"
+        dateTextField?.text = dateFormatter.stringFromDate(timePicker.date)
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(okAction)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
 }
+
+
